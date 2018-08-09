1eb46eb8963cdff2b29de6fc04675460ac5aa9fe
niejun <niejun@qq.com>
2018-08-09 18:03:12 +0800

    提交

---
+++ app/src/main/java/com/nj/treedemo/TreeNodePid.java
@@ -0,0 +1,15 @@
+package com.nj.treedemo;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2018-08-09.
+ */
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TreeNodePid {
+}
